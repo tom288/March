09df6dd798551c247e796fc66fdab617a248106f
Tom <tombo.miller@gmail.com>
2019-06-05 00:47:16 +0100

    More cosmetic changes to chunk.hpp

--- src/chunk.hpp
+++ src/chunk.hpp
@@ -7,42 +7,6 @@
 #include "osn.h"
 
 // TODO
-static const int edges[256] = {
-   0x000,  0x109,  0x203,  0x30a,  0x406,  0x50f,  0x605,  0x70c,
-   0x80c,  0x905,  0xa0f,  0xb06,  0xc0a,  0xd03,  0xe09,  0xf00,
-   0x190,  0x099,  0x393,  0x29a,  0x596,  0x49f,  0x795,  0x69c,
-   0x99c,  0x895,  0xb9f,  0xa96,  0xd9a,  0xc93,  0xf99,  0xe90,
-   0x230,  0x339,  0x033,  0x13a,  0x636,  0x73f,  0x435,  0x53c,
-   0xa3c,  0xb35,  0x83f,  0x936,  0xe3a,  0xf33,  0xc39,  0xd30,
-   0x3a0,  0x2a9,  0x1a3,  0x0aa,  0x7a6,  0x6af,  0x5a5,  0x4ac,
-   0xbac,  0xaa5,  0x9af,  0x8a6,  0xfaa,  0xea3,  0xda9,  0xca0,
-   0x460,  0x569,  0x663,  0x76a,  0x066,  0x16f,  0x265,  0x36c,
-   0xc6c,  0xd65,  0xe6f,  0xf66,  0x86a,  0x963,  0xa69,  0xb60,
-   0x5f0,  0x4f9,  0x7f3,  0x6fa,  0x1f6,  0x0ff,  0x3f5,  0x2fc,
-   0xdfc,  0xcf5,  0xfff,  0xef6,  0x9fa,  0x8f3,  0xbf9,  0xaf0,
-   0x650,  0x759,  0x453,  0x55a,  0x256,  0x35f,  0x055,  0x15c,
-   0xe5c,  0xf55,  0xc5f,  0xd56,  0xa5a,  0xb53,  0x859,  0x950,
-   0x7c0,  0x6c9,  0x5c3,  0x4ca,  0x3c6,  0x2cf,  0x1c5,  0x0cc,
-   0xfcc,  0xec5,  0xdcf,  0xcc6,  0xbca,  0xac3,  0x9c9,  0x8c0,
-   0x8c0,  0x9c9,  0xac3,  0xbca,  0xcc6,  0xdcf,  0xec5,  0xfcc,
-   0x0cc,  0x1c5,  0x2cf,  0x3c6,  0x4ca,  0x5c3,  0x6c9,  0x7c0,
-   0x950,  0x859,  0xb53,  0xa5a,  0xd56,  0xc5f,  0xf55,  0xe5c,
-   0x15c,  0x055,  0x35f,  0x256,  0x55a,  0x453,  0x759,  0x650,
-   0xaf0,  0xbf9,  0x8f3,  0x9fa,  0xef6,  0xfff,  0xcf5,  0xdfc,
-   0x2fc,  0x3f5,  0x0ff,  0x1f6,  0x6fa,  0x7f3,  0x4f9,  0x5f0,
-   0xb60,  0xa69,  0x963,  0x86a,  0xf66,  0xe6f,  0xd65,  0xc6c,
-   0x36c,  0x265,  0x16f,  0x066,  0x76a,  0x663,  0x569,  0x460,
-   0xca0,  0xda9,  0xea3,  0xfaa,  0x8a6,  0x9af,  0xaa5,  0xbac,
-   0x4ac,  0x5a5,  0x6af,  0x7a6,  0x0aa,  0x1a3,  0x2a9,  0x3a0,
-   0xd30,  0xc39,  0xf33,  0xe3a,  0x936,  0x83f,  0xb35,  0xa3c,
-   0x53c,  0x435,  0x73f,  0x636,  0x13a,  0x033,  0x339,  0x230,
-   0xe90,  0xf99,  0xc93,  0xd9a,  0xa96,  0xb9f,  0x895,  0x99c,
-   0x69c,  0x795,  0x49f,  0x596,  0x29a,  0x393,  0x099,  0x190,
-   0xf00,  0xe09,  0xd03,  0xc0a,  0xb06,  0xa0f,  0x905,  0x80c,
-   0x70c,  0x605,  0x50f,  0x406,  0x30a,  0x203,  0x109,  0x000
-};
-
-// TODO
 static const int tris[256][16] = {
    { -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
    {  0,  8,  3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1 },
@@ -374,44 +338,28 @@ private:
    osn_context* ctx;
 
    // Generates the mesh for position pos
-   void polygonise(glm::vec3 pos, double isolevel)
+   void polygonise(glm::vec3 pos, double lvl)
    {
       // Populate the grid array
       glm::vec3 grid[8];
-      for (int g = 0; g < 8; g++)
+      for (int g = 0; g < 8; g++) // TODO check
       {
          grid[g] = pos;
-         for (int d = 0; d < 3; d++) grid[g][d] += (g >> d) & 1;
+         for (int d = 0; d < 3; d++) grid[g][d] += g >> d & 1;
       }
 
       // Determine the configuration index
-      int cubeindex = 0;
-      for (int g = 0; g < 8; g++)
-      {
-         if (sample(grid[g]) < isolevel) cubeindex |= 1 << g;
-      }
-
-      // Return if the volume contains 0 triangles
-      if (!edges[cubeindex]) return;
-
-      // List the vertices
-      glm::vec3 vertlist[12];
-      int other[12] = { 1, 2, 3, 0, 5, 6, 7, 4, 4, 5, 6, 7 };
-      for (int e = 0; e < 12; e++)
-      {
-         if (edges[cubeindex] & (1 << e))
-         {
-            vertlist[e] = verterp2(isolevel, grid[e % 8], grid[other[e]]);
-         }
-      }
+      int config = 0;
+      for (int g = 0; g < 8; g++) if (sample(grid[g]) < lvl) config |= 1 << g;
 
       // Create the triangles
-      for (int v = 0; tris[cubeindex][v] != -1; v++)
+      const int OTHER[12] = { 1, 2, 3, 0, 5, 6, 7, 4, 4, 5, 6, 7 };
+      for (int t, v = 0; (t = tris[config][v]) != -1; v++)
       {
          // Vertex position data
          for (int d = 0; d < 3; d++)
          {
-            verts.push_back(vertlist[tris[cubeindex][v]][d]);
+            verts.push_back(verterp2(lvl, grid[t % 8], grid[OTHER[t]])[d]);
          }
 
          // Vertex colour data TODO use proper values instead of prime hashes
@@ -421,27 +369,24 @@ private:
       }
    }
 
-   // Lerps for edge intersections
-   glm::vec3 verterp(double isolevel, glm::vec3 a, glm::vec3 b) const
+   // TODO
+   glm::vec3 verterp(double lvl, glm::vec3 a, glm::vec3 b) const
    {
-      if (glm::abs(isolevel  - sample(a)) < 0.00001) return a;
-      if (glm::abs(isolevel  - sample(b)) < 0.00001) return b;
-      if (glm::abs(sample(a) - sample(b)) < 0.00001) return a;
+      if (glm::abs(lvl       - sample(a)) < 1e-5) return a;
+      if (glm::abs(lvl       - sample(b)) < 1e-5) return b;
+      if (glm::abs(sample(a) - sample(b)) < 1e-5) return a;
 
-      glm::vec3 r;
-      double mu = (isolevel - sample(a)) / (sample(b) - sample(a));
-      for (int d = 0; d < 3; d++) r[d] = a[d] + mu * (b[d] - a[d]);
-      return r;
+      return a + float((lvl - sample(a)) / (sample(b) - sample(a))) * (b - a);
    }
 
    // TODO
-   glm::vec3 verterp2(double isolevel, glm::vec3 a, glm::vec3 b) const
+   glm::vec3 verterp2(double lvl, glm::vec3 a, glm::vec3 b) const
    {
       if (less(b, a)) std::swap(b, a);
       float s = sample(b) - sample(a);
 
       glm::vec3 r = a;
-      if (glm::abs(s) > 1e-5) r += (b - a) / s * float(isolevel - sample(a));
+      if (glm::abs(s) > 1e-5) r += (b - a) / s * float(lvl - sample(a));
       return r;
    }
 
